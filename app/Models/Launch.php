ca1f4840dd859654a2601fd1c73aa341db8eb01d
Jorge Carlos <skay_1994@yahoo.com.br>
2023-06-27 11:59:20 -0300

    Add Launch fillable and casts

--- app/Models/Launch.php
+++ app/Models/Launch.php
@@ -2,8 +2,25 @@
 
 namespace App\Models;
 
+use App\Enums\Status;
 use Illuminate\Database\Eloquent\Model;
 
 class Launch extends Model
 {
+    protected $fillable = [
+        'uuid', 'status', 'url', 'launch_library_id', 'name', 'slug', 'net', 'window_start', 'window_end',
+        'inhold', 'tbdtime', 'tbddate', 'probability', 'holdreason', 'failreason', 'hashtag', 'webcast_live',
+        'image', 'infographic', 'program'
+    ];
+
+    protected $casts = [
+        'inhold' => 'boolean',
+        'webcast_live' => 'boolean',
+        'status' => Status::class,
+        'net' => 'datetime',
+        'window_start' => 'datetime',
+        'window_end' => 'datetime',
+        'tbdtime' => 'datetime',
+        'tbddate' => 'datetime',
+    ];
 }
