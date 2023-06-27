bb9e8abc78e1308bed45d16a70cca8425968c0f7
Jorge Carlos <skay_1994@yahoo.com.br>
2023-06-27 14:05:13 -0300

    Add Mission relation to Orbit model

--- app/Models/Orbit.php
+++ app/Models/Orbit.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Orbit extends Model
 {
@@ -12,4 +13,9 @@ class Orbit extends Model
     protected $fillable = [
         'name', 'abbrev'
     ];
+
+    public function missions(): HasMany
+    {
+        return $this->hasMany(Mission::class);
+    }
 }
