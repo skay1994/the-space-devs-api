0cee05f8b553442b06cc5306e70ee3d4b42f1c22
Jorge Carlos <skay_1994@yahoo.com.br>
2023-06-29 17:11:52 -0300

    Add LaunchController

---
+++ app/Http/Controllers/LaunchController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\LaunchResource;
+use App\Models\Launch;
+use Illuminate\Http\Request;
+
+class LaunchController extends Controller
+{
+    public function index(Request $request)
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Launch $launch)
+    {
+        //
+    }
+
+    public function update(Request $request, Launch $launch)
+    {
+        //
+    }
+
+    public function destroy(Launch $launch)
+    {
+        //
+    }
+}
