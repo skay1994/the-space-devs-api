3cdd3893011cb1759944fdb5659a2ad7084340c5
Jorge Carlos <skay_1994@yahoo.com.br>
2023-06-29 17:09:16 -0300

    Add route bind for Launch model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Launch;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Route::model('launcher', Launch::class);
     }
 
     /**
