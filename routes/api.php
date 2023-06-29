23696056c9549c2d6ffea96984b271c834b9b432
Jorge Carlos <skay_1994@yahoo.com.br>
2023-06-29 14:03:13 -0300

    Add routes for login and logout and

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('login', LoginController::class)->name('api.login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->as('api.')->group(function () {
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::name('profile')->get('/user', static function (Request $request) {
+        return $request->user();
+    });
 });
