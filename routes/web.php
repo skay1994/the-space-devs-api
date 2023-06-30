82ba0942d6c2e8056ecd998293ef22f598e3ad1a
Jorge Carlos <skay_1994@yahoo.com.br>
2023-06-30 01:24:10 -0300

    Change base route content

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', static fn() => "REST Back-end Challenge 20201209 Running");
