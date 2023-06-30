8310e4db0bdd86834131b913538538dda9c54610
Jorge Carlos <skay_1994@yahoo.com.br>
2023-06-30 01:23:42 -0300

    Update Status enum with new status

--- app/Enums/Status.php
+++ app/Enums/Status.php
@@ -2,26 +2,18 @@
 
 namespace App\Enums;
 
-enum Status: int
+enum Status: string
 {
-    case GO = 1;
-    case TBD = 2;
-    case SUCCESS = 3;
-    case FAILURE = 4;
-    case HOLD = 5;
-    case LAUNCH_FLIGHT = 6;
-    case PARTIAL_FAILURE = 7;
+    case DRAFT = 'draft';
+    case TRASH = 'trash';
+    case PUBLISHED = 'published';
 
     public function name(): string
     {
         return match ($this) {
-            self::GO => trans('Go for Launch'),
-            self::TBD => trans('To Be Confirmed'),
-            self::SUCCESS => trans('Success'),
-            self::FAILURE => trans('Failed'),
-            self::HOLD => trans('Hold'),
-            self::LAUNCH_FLIGHT => trans('Launch on Flight'),
-            self::PARTIAL_FAILURE => trans('Launch was a Partial Failure'),
+            self::DRAFT => trans('Draft'),
+            self::TRASH => trans('Trash'),
+            self::PUBLISHED => trans('Published'),
         };
     }
 
