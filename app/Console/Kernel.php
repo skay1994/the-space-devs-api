9bd116b25428b0c32e303798d67b74d745191633
Jorge Carlos <skay_1994@yahoo.com.br>
2023-07-02 15:41:56 -0300

    Changed schedule time by 1 hour to 5 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          $schedule->command('the-space-devs:launch-import')
-             ->hourly()
+             ->everyFiveMinutes()
              ->when(fn () => Launch::count('uuid') < 2000);
     }
 
